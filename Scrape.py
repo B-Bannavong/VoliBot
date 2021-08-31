390d84c1302255732856cf3992d122609d6ca260
ArvoDart <checorozco@gmail.com>
2021-08-30 22:04:35 -0400

    cleaned up the code a lil

--- Scrape.py
+++ Scrape.py
@@ -14,32 +14,42 @@ data = {
     'username': 'Eccccca',
     'password': pw
 }
+subs = ['https://oauth.reddit.com/r/wallstreetbets/hot', 'https://oauth.reddit.com/r/smallstreetbets', 'https://oauth.reddit.com/r/investing']
 headers = {'User-Agent' : 'MyAPI/0.0.1'}
 res = requests.post('https://www.reddit.com/api/v1/access_token',
 auth=HTTPBasicAuth(CLIENT_ID, SECRET_KEY), data=data, headers=headers)
 TOKEN = res.json()['access_token']
 headers['Authorization'] = f'bearer {TOKEN}'
-res = requests.get('https://oauth.reddit.com/r/wallstreetbets/hot',
-headers=headers, params={'limit' : '50'}) #We can add 'after' : [id]' to get all posts after the specified ID
 
-df = pd.DataFrame()
-nlp = spacy.load('en_core_web_sm')
-for post in res.json()['data']['children']:
-    df = df.append({
-        'Subreddit' : post['data']['subreddit'],
-        'Type' : post['kind'] + '_' + post['data']['id'],
-        'Flair' : post['data']['link_flair_text'],
-        'Title' : post['data']['title'],
-        'Post_Text' : post['data']['selftext'],       
-        'Upvotes' : post['data']['ups'],
-        'Downvotes' : post['data']['downs'],
-        'Ratio' : post['data']['upvote_ratio'],
-    }, ignore_index=True)
 
-options = ['DD', 'YOLO', 'Gain', 'Technical Analysis']
-df_result = df[df['Flair'].isin(options)]
+def main(reddit_url):
+    for url in reddit_url:
+        res = requests.get(url,
+        headers=headers, params={'limit' : '50'}) #We can add 'after' : [id]' to get all posts after the specified ID
 
-BLACKLIST = ['otm', 'itm', 'yolo', 'ath']
+        df = pd.DataFrame()
+        nlp = spacy.load('en_core_web_sm')
+        for post in res.json()['data']['children']:
+            df = df.append({
+                'Subreddit' : post['data']['subreddit'],
+                'Type' : post['kind'] + '_' + post['data']['id'],
+                'Flair' : post['data']['link_flair_text'],
+                'Title' : post['data']['title'],
+                'Post_Text' : post['data']['selftext'],       
+                'Upvotes' : post['data']['ups'],
+                'Downvotes' : post['data']['downs'],
+                'Ratio' : post['data']['upvote_ratio'],
+            }, ignore_index=True)
+
+        options = ['DD', 'YOLO', 'Gain', 'Technical Analysis']
+        df_result = df[df['Flair'].isin(options)]
+
+        BLACKLIST = ['otm', 'itm', 'yolo', 'ath']
+
+
+        df_result['Tickers'] = df_result['Title'].apply(get_tckr)
+        print(df_result)
+
 def get_tckr(text):
     doc = nlp(text)
     #displacy.render(doc, style='ent')
@@ -48,9 +58,8 @@ def get_tckr(text):
         if entity.label_ == 'ORG' and entity.text.lower() not in BLACKLIST:
             tckr_list.append(entity.text)
     return (tckr_list)
-
-df_result['Tickers'] = df_result['Title'].apply(get_tckr)
-print(df_result)
+if __name__ == "__main__":
+    main(subs)
 
 
 #print(post['data'].keys()) #(call this for list of possible keys)
