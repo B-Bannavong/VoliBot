0ec00bee488c2919c64b1a8507639e8d2ec597ba
Brandon <brandonbannavong2355@gmail.com>
2021-08-31 20:37:53 -0400

    added additional flair filter options

--- Scrape.py
+++ Scrape.py
@@ -12,12 +12,12 @@ data = {
     'username': 'Eccccca',
     'password': 'brandonb10'
 }
-subs = ['https://oauth.reddit.com/r/wallstreetbets/hot', 'https://oauth.reddit.com/r/smallstreetbets', 'https://oauth.reddit.com/r/investing']
+subs = ['https://oauth.reddit.com/r/wallstreetbets/hot', 'https://oauth.reddit.com/r/smallstreetbets/hot', 'https://oauth.reddit.com/r/investing/hot']
 headers = {'User-Agent' : 'MyAPI/0.0.1'}
 BLACKLIST = ['otm', 'itm', 'yolo', 'ath']
 CLIENT_ID = 'kOUB-9yoTlH7vNtmDDcMPw'
 SECRET_KEY = 'fG_oxXT93FZLxMVmN-Bpmpq8j30yyQ'
-options = ['DD', 'YOLO', 'Gain', 'Technical Analysis']
+options = ['DD', 'YOLO', 'Gain', 'Technical Analysis', 'Discussion', 'News', 'YOLOOO', 'Epic DD Analysis']
 
 class scraper:
     
@@ -50,7 +50,8 @@ class scraper:
                 }, ignore_index=True)
 
 
-            df_result = self.df[self.df['Flair'].isin(options)]
+            if url != 'https://oauth.reddit.com/r/investing/hot':
+                df_result = self.df[self.df['Flair'].isin(options)]
 
 
 
