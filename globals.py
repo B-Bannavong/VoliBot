0049741acf910ca1f178a593862c8205021ca2b5
ArvoDart <checorozco@gmail.com>
2021-09-06 00:40:15 -0400

    updated globals

--- globals.py
+++ globals.py
@@ -8,6 +8,13 @@ data = {
     'username': 'Eccccca',
     'password': 'brandonb10'
 }
+subs = ['https://oauth.reddit.com/r/wallstreetbets/hot', 'https://oauth.reddit.com/r/smallstreetbets/hot', 'https://oauth.reddit.com/r/investing/hot','https://oauth.reddit.com/r/stocks/hot' ]
+
+headers = {'User-Agent' : 'MyAPI/0.0.1'}
+BLACKLIST = ['otm', 'itm', 'yolo', 'ath']
+CLIENT_ID = 'kOUB-9yoTlH7vNtmDDcMPw'
+SECRET_KEY = 'fG_oxXT93FZLxMVmN-Bpmpq8j30yyQ'
+options = ['DD', 'YOLO', 'Gain', 'Technical Analysis', 'Discussion', 'News', 'YOLOOO', 'Epic DD Analysis', 'Company News', 'Industry Discussion', 'Company Discussion', 'Trades', 'Company Question']
 
 
 
